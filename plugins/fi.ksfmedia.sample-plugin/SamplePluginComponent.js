6edaa6ded64d3b49cb585c614b7833e51c6200c1
Joakim Ekroos <joakim.ekroos@ksfmedia.fi>
2019-07-02 14:25:50 +0300

    Make testplugindiv red

--- plugins/fi.ksfmedia.sample-plugin/SamplePluginComponent.js
+++ plugins/fi.ksfmedia.sample-plugin/SamplePluginComponent.js
@@ -5,11 +5,13 @@ import './scss/index.scss'
 class SamplePluginComponent extends Component {
     constructor(...args) {
       super(...args)
+      console.log('Testing Sample Plugin')
     }
     render($$) {
-        const el = $$('div').addClass('testclass'),
+        const el = $$('div'),
           label = $$('h2').append('test')
 
+        el.id = 'testplugindiv'
         el.append(label)
 
         return el
